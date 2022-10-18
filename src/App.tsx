9f344821b713cbd2b20dd3e43ed52cd662298743
mr_darcy <mr_darcy_1@mail.ru>
2022-10-18 22:31:29 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import './App.css';
 import {Button} from "./Components/Button";
+
 function App() {
-    const Button1foo =(subscriber:string, age:number)=> {
-        console.log(subscriber,age)
+    const Button1foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
     }
-    const Button2foo =(subscriber:string,age:number)=> {
-        console.log(subscriber,age)
+    const Button2foo = (subscriber: string, age: number) => {
+        console.log(subscriber, age)
     }
-    const Button3foo =()=> {
+    const Button3foo = () => {
         console.log('I am stupid button')
     }
-  return (
-    <div className="App">
-        <Button name={'MyYouTubeChannel-1'} callBack={()=>Button1foo('I am Vasya',21)}/>
-        <Button name={'MyYouTubeChannel-12'}  callBack={()=>Button2foo('I am Vanya',24)}/>
-        <Button name={'StupidButton'} callBack={Button3foo}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button name={'MyYouTubeChannel-1'} callBack={() => Button1foo('I am Vasya', 21)}/>
+            <Button name={'MyYouTubeChannel-12'} callBack={() => Button2foo('I am Vanya', 24)}/>
+            <Button name={'StupidButton'} callBack={Button3foo}/>
+        </div>
+    );
 }
 
 export default App;
