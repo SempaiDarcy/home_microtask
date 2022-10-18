b53efb971dfc64550b3156fface487862584f08d
mr_darcy <one.akbar@mail.ru>
2022-10-18 19:13:57 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import './App.css';
 import {Button} from "./Components/Button";
 function App() {
+    const Button1foo =(subscriber:string, age:number)=> {
+        console.log(subscriber,age)
+    }
+    const Button2foo =(subscriber:string,age:number)=> {
+        console.log(subscriber,age)
+    }
+    const Button3foo =()=> {
+        console.log('I am stupid button')
+    }
   return (
     <div className="App">
-        <Button name={'MyYouTubeChannel-1'}/>
-        <Button name={'MyYouTubeChannel-12'} />
+        <Button name={'MyYouTubeChannel-1'} callBack={()=>Button1foo('I am Vasya',21)}/>
+        <Button name={'MyYouTubeChannel-12'}  callBack={()=>Button2foo('I am Vanya',24)}/>
+        <Button name={'StupidButton'} callBack={Button3foo}/>
     </div>
   );
 }
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 type ButtonType = {
     name: string
+    callBack:()=> void
 }
 
  export const Button = (props:ButtonType) => {
+    const onclickHandler = () => {
+        props.callBack()
+    }
     return (
         <div>
-            <button>{props.name}</button>
+            <button onClick={onclickHandler}>{props.name}</button>
         </div>
     );
 };
