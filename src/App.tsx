0dcc5aab8e454e4afb005a48b81127a8534851f6
mr_darcy <mr_darcy_1@mail.ru>
2022-10-18 23:57:20 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Button} from "./Components/Button";
+// import {Button} from "./Components/Button";
 
 function App() {
-    const Button1foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-    const Button2foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-    const Button3foo = () => {
-        console.log('I am stupid button')
-    }
+    // const Button1foo = (subscriber: string, age: number) => {
+    //     console.log(subscriber, age)
+    // }
+    // const Button2foo = (subscriber: string, age: number) => {
+    //     console.log(subscriber, age)
+    // }
+    // const Button3foo = () => {
+    //     console.log('I am stupid button')
+    // }
+    // let [a,setA]=useState(1)
+    // const onclickHandler = () => {
+    //     setA(++a);
+    //     console.log(a)
+    // }
+    // const onclickHandler2= () =>{
+    //     setA(0);
+    // }
     return (
         <div className="App">
-            <Button name={'MyYouTubeChannel-1'} callBack={() => Button1foo('I am Vasya', 21)}/>
-            <Button name={'MyYouTubeChannel-12'} callBack={() => Button2foo('I am Vanya', 24)}/>
-            <Button name={'StupidButton'} callBack={Button3foo}/>
+
+            {/*<h1>{a}</h1>*/}
+            {/*<button onClick={onclickHandler}>number</button>*/}
+            {/*<button onClick={onclickHandler2}>0</button>*/}
+            {/*<Button name={'MyYouTubeChannel-1'} callBack={() => Button1foo('I am Vasya', 21)}/>*/}
+            {/*<Button name={'MyYouTubeChannel-12'} callBack={() => Button2foo('I am Vanya', 24)}/>*/}
+            {/*<Button name={'StupidButton'} callBack={Button3foo}/>*/}
         </div>
     );
 }
