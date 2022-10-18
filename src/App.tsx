077b67fa03c07440c6b416a3fcfe91aebc143631
mr_darcy <one.akbar@mail.ru>
2022-10-18 17:54:18 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
 import React from 'react';
 import './App.css';
-import NewComponent from "./NewComponent";
-
+import {Button} from "./Components/Button";
 function App() {
-    // const topCars = [
-    //     {manufacturer:'BMW', model:'m5cs'},
-    //     {manufacturer:'Mercedes', model:'e63s'},
-    //     {manufacturer:'Audi', model:'rs6'}
-    // ]
-
-    // const MyFirstSubscriber = ()=>{
-    //     console.log('Hello, I am a Vasya')
-    // }
-    // const MySecondSubscriber = ()=>{
-    //     console.log('Hello, I am Ivan')
-    // }
-    //
-    const onclickHandler = (name:string) => {
-        console.log(name)
-    }
-    // const foo1 = () => {
-    //     console.log(100200)
-    // }
-    // const foo2 = (number:number) => {
-    //     console.log(number)
-    // }
   return (
-    <div>
-        {/*<NewComponent topCars={topCars}/>*/}
-        {/*<button onClick={(event)=>{console.log('Hellow')}}>MyYouTubeChannel-1</button>*/}
-        <button onClick={()=>onclickHandler('Vasya')}>MyYouTubeChannel-1</button>
-        <button onClick={()=>onclickHandler('Ivan')}>MyYouTubeChannel-2</button>
-        {/*<button onClick={foo1}>1</button>*/}
-        {/*<button onClick={()=>foo2(100200)}>2</button>*/}
+    <div className="App">
+        <Button/>
+        <button>MyYouTubeChannel-2</button>
     </div>
   );
 }
---
+++ src/Components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+ export const Button = () => {
+    return (
+        <div>
+            <button>MyYouTubeChannel-1</button>
+        </div>
+    );
+};
+
