065757a9e67b6efb804cd906fef5e88b33eed1ec
mr_darcy <mr_darcy_1@mail.ru>
2022-10-24 05:39:23 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import './App.css';
 import {NewComponent} from "./components/NewComponent";
 
 
+
+const topCars = [
+    {manufacturer:'BMW', model:'m5cs'},
+    {manufacturer:'Mercedes', model:'e63s'},
+    {manufacturer:'Audi', model:'rs6'}
+]
+
 function App() {
     return (
         <>
-            <NewComponent/>
+            <NewComponent carsTop={topCars}/>
         </>
     )
 }
--- src/components/NewComponent.tsx
+++ src/components/NewComponent.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
-export const NewComponent = () => {
+type NewComponentType = {
+    carsTop: Array<CarsType>
+}
+
+type CarsType = {
+    manufacturer:string,
+    model:string
+}
+
+
+export const NewComponent = (props:NewComponentType) => {
     return (
-        <div>
-            new component
-        </div>
+        <th>
+            {props.carsTop.map((o,index) => {
+                return (
+                    <tr key={index}>
+                        <span>Индекс: {index} ;</span>
+                        <span> Name: {o.manufacturer} ;</span>
+                        <span> Model: {o.model} ;</span>
+                    </tr>
+                )
+                }
+            )
+            }
+        </th>
     );
 };
 
