4c238d1fe244467a74f8657f90edb08521798b90
mr_darcy <mr_darcy_1@mail.ru>
2022-10-24 18:12:33 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React,{MouseEvent} from 'react';
 import './App.css';
 
-
-
-
 function App() {
-    return (
-        <>
+    const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+        console.log('Hello Im VASYA!')
+    }
+    const mySecondSubscriber = () => {
+        console.log('Hello Im IVAN!')
+    }
 
-        </>
+    return (
+        <div className='App'>
+            {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChanel-1</button>*/}
+            <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
+            <button onClick={mySecondSubscriber}>MyYouTubeChanel-2</button>
+        </div>
     )
 }
-
 export default App;
