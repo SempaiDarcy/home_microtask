2086d2aaf2150df5605e93281e3dac952d1f5c26
mr_darcy <mr_darcy_1@mail.ru>
2022-10-24 18:27:43 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React,{MouseEvent} from 'react';
 import './App.css';
 
 function App() {
-    const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-        console.log('Hello Im VASYA!')
-    }
-    const mySecondSubscriber = () => {
-        console.log('Hello Im IVAN!')
+    // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello Im VASYA!')
+    // }
+    // const mySecondSubscriber = () => {
+    //     console.log('Hello Im IVAN!')
+    // }
+    const onClickHandler =(name:string) => {
+        console.log(name)
     }
 
     return (
         <div className='App'>
             {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChanel-1</button>*/}
-            <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
-            <button onClick={mySecondSubscriber}>MyYouTubeChanel-2</button>
+            <button onClick={(event)=>onClickHandler('VASYA')}>MyYouTubeChanel-1</button>
+            <button onClick={(event)=>onClickHandler('IVAN')}>MyYouTubeChanel-2</button>
         </div>
     )
 }
