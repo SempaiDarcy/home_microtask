3fef48882a5eb32bce26c8fc93aee98aebcb96f1
mr_darcy <mr_darcy_1@mail.ru>
2022-10-30 16:37:43 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,27 @@
 import React, {useState} from 'react';
 import './App.css';
-import {FullInput} from "./components/SecondWeek/FullInput";
-import {Input} from "./components/SecondWeek/Input";
-import {Button} from "./components/SecondWeek/Button";
 
 function App() {
-    let [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'}
+    let [message,setMessage] = useState([
+        {message:'message1'},
+        {message:'message2'},
+        {message:'message3'},
+        {message:'message4'}
     ])
+ return (
+     <div className={'App'}>
+         <div>
+             <input/>
+             <button>+</button>
+         </div>
+         {message.map((el,index)=>{
+             return (
+                 <div key={index}>{el.message}</div>
+             )
 
-     let [title,setTitle]=useState('')
-    console.log(title)
-
-    const callBackButtonHandler = () => {
-       addMessage(title)
-        setTitle('')
-    }
-
-    const addMessage = (title: string) => {
-        let newMessage = {message: title}
-        setMessage([newMessage,...message])
-    }
-
-    return (
-        <div className={'App'}>
-            {/*<FullInput addMessage={addMessage}/>*/}
-            <Input setTitle = {setTitle} title={title}/>
-            <Button name={'+'} callBack={()=>{callBackButtonHandler()}}/>
-            {message.map((el, index) => {
-                return (
-                    <div key={index}>
-                        {el.message}
-                    </div>
-                )
-            })}
-        </div>
-    )
+         })}
+     </div>
+ )
 }
 
 export default App;
