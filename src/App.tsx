07c6ba643e1e0ae964a734c87557622df1be3292
mr_darcy <mr_darcy_1@mail.ru>
2022-10-30 15:01:33 +0500

    Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import './App.css';
 import {FullInput} from "./components/SecondWeek/FullInput";
+import {Input} from "./components/SecondWeek/Input";
+import {Button} from "./components/SecondWeek/Button";
 
 function App() {
     let [message, setMessage] = useState([
@@ -8,6 +10,15 @@ function App() {
         {message: 'message2'},
         {message: 'message3'}
     ])
+
+     let [title,setTitle]=useState('')
+    console.log(title)
+
+    const callBackButtonHandler = () => {
+       addMessage(title)
+        setTitle('')
+    }
+
     const addMessage = (title: string) => {
         let newMessage = {message: title}
         setMessage([newMessage,...message])
@@ -15,7 +26,9 @@ function App() {
 
     return (
         <div className={'App'}>
-            <FullInput addMessage={addMessage}/>
+            {/*<FullInput addMessage={addMessage}/>*/}
+            <Input setTitle = {setTitle} title={title}/>
+            <Button name={'+'} callBack={()=>{callBackButtonHandler()}}/>
             {message.map((el, index) => {
                 return (
                     <div key={index}>
